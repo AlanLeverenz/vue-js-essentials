d6dba70051e1157c14821fc019731bf423095a7e
Alan Leverenz <awleverenz@aol.com>
2021-02-08 11:39:15 -0500

    fixed main.js file

--- video-browser/src/main.js
+++ video-browser/src/main.js
@@ -1,3 +1,25 @@
+// import Vue from 'vue';
+// import App from './App';
+
+/*
+new Vue ({
+    el: '#app',
+    render: function(createElement) {
+        return createElement(App);
+    }
+});
+*/
+
+/*
+new Vue({
+    render: h => h(App)
+}).$mount('#app');
+*/
+
+/*
+Vue.createApp(App).mount('#app')
+*/
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
