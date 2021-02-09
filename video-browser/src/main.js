f44b45874ef2c91dcf1a3ee50f279ec6e9a39a55
Alan Leverenz <awleverenz@aol.com>
2021-02-08 21:43:32 -0500

    added SearchBar.vue file in components

--- video-browser/src/main.js
+++ video-browser/src/main.js
@@ -1,7 +1,8 @@
-// import Vue from 'vue';
-// import App from './App';
-
+// *****************
 /*
+import Vue from 'vue';
+import App from './App';
+
 new Vue ({
     el: '#app',
     render: function(createElement) {
@@ -30,3 +31,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
