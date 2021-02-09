71b9205d917d83edfc4ec18b548045ee9be3e544
Alan Leverenz <awleverenz@aol.com>
2021-02-09 13:22:33 -0500

    wired component search event to App.vue as directive and method

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
-        <SearchBar >
-        </SearchBar>
+        <SearchBar @termChange="onTermChange"></SearchBar>
     </div>
 </template>
 
@@ -10,10 +9,16 @@ import SearchBar from './components/SearchBar';
 // insert component tag inside template for html viewing
 // components object has key and value
 // can shorten to single word if key and value are the same
+// @ is in place of v-on: directive syntax
 export default {
     name: 'App',
     components: {
         SearchBar
+    },
+    methods: {
+        onTermChange(searchTerm) {
+            console.log(searchTerm);
+        }
     }
 };
 </script>
