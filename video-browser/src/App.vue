0c5dcd8a2f8688ec306f53244390f6aa06533467
Alan Leverenz <awleverenz@aol.com>
2021-02-08 21:54:08 -0500

    added components in SearchBar script

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
-        Hi there!
+        <SearchBar >
+        </SearchBar>
     </div>
 </template>
 
 <script>
+import SearchBar from './components/SearchBar';
+// insert component tag inside template for html viewing
+// components object has key and value
+// can shorten to single word if key and value are the same
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        SearchBar
+    }
 };
 </script>
