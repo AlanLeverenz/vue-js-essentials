08f02bcfc589690ee2e6dc965f6a84b92028b98c
Alan Leverenz <awleverenz@aol.com>
2021-02-10 12:39:03 -0500

    inserted VideoListItem component

--- video-browser/src/components/VideoList.vue
+++ video-browser/src/components/VideoList.vue
@@ -10,8 +10,13 @@
 // props: ['videos'] is an array element
 // props: { videos: } is an array of object
 // Array sets the type
+import VideoListItem from './VideoListItem';
+
 export default {
     name: 'VideoList',
+    components: {
+        VideoListItem
+    },
     props: ['videos']
 }
 </script>
