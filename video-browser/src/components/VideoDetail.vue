294b0044dad9cd9b7261480c04f9a26c05c0a248
Alan Leverenz <awleverenz@aol.com>
2021-02-14 13:14:57 -0500

    added iframe for youtube video, details still not appearing

--- video-browser/src/components/VideoDetail.vue
+++ video-browser/src/components/VideoDetail.vue
@@ -1,5 +1,8 @@
 <template>
     <div v-if="video">
+        <div>
+            <iframe :src="videoUrl" />
+        </div>
         <div class="details">
             <h4>{{ video.snippet.title }}</h4>
             <p>{{ video.snippet.description }}</p>
@@ -11,7 +14,15 @@
 // {{ }} holds a variable object that needs interpreting
 export default {
     name: 'VideoDetail',
-    props: ['video']
+    props: ['video'],
+    computed: {
+        videoUrl() {
+            // const videoId = this.video.id.videoId;
+            // ES2015 pull object from the parent object
+            const { videoId } = this.video.id;
+            return `https://www.youtube.com/embed/${videoId}`;
+        }
+    }
 }
 </script>
 
