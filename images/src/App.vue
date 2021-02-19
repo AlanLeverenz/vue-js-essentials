3b60044bb8649cd916043befa55f0c41ec071a59
Alan Leverenz <awleverenz@aol.com>
2021-02-19 14:09:24 -0500

    added AppHeader

--- images/src/App.vue
+++ images/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-    <h3>My Images</h3>
+    <div>
+        <AppHeader></AppHeader>
+    </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader';
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        AppHeader
+    }
 };
 </script>
