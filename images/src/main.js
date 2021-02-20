9ca69dee2c4b4240d823baefff0c8f79d9deb549
Alan Leverenz <awleverenz@aol.com>
2021-02-20 13:22:40 -0500

    connected vue to vuex

--- images/src/main.js
+++ images/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import App from './App';
+// webpack assume there is an index.js file inside store
+import store from './store';
 
+// store is included in Vue instance to provide data
 new Vue({
+    store,
     render: h => h(App)
 }).$mount('#app');
 
---
+++ images/src/store/index.js
@@ -0,0 +1,9 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+// allows Vuex to talk to Vue
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: {}
+});
