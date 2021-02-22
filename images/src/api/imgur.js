b2aae5248bb07d183cc95f57ec9f6b0cac883eb1
Alan Leverenz <awleverenz@aol.com>
2021-02-21 21:07:30 -0500

    created imgur querystring and url

---
+++ images/src/api/imgur.js
@@ -0,0 +1,18 @@
+import { myAPI } from "../../api";
+
+import qs from 'qs';
+
+
+const CLIENT_ID = myAPI.Client_ID;
+const ROOT_URL = myAPI.Root_URL;
+
+export default {
+    login() {
+        const querystring = {
+            client_id: CLIENT_ID,
+            response_type: 'token'
+        };
+        // window.location causes user's browser to navigate to this URL
+        window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
+    }
+};
