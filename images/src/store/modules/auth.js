f76c62515013f7ed9c9e08f4b84a31845b5bd317
Alan Leverenz <awleverenz@aol.com>
2021-02-22 16:04:07 -0500

    defines finalizeLogin portion of actions in auth.js

--- images/src/store/modules/auth.js
+++ images/src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import api from '../../api/imgur';
+import qs from 'qs';
 
 const state = {
     token: null
@@ -15,6 +16,14 @@ const getters = {
 const actions = {
     login: () => {
         api.login();
+    },
+    finalizeLogin({ commit }, hash) {
+        // finalizeLogin carries the hash (2nd argument to action)
+        // cleans up the query response
+        const query = qs.parse(hash.replace('#', ''));
+        // gets the access_token string from query
+        // updates the 'token' state
+        commit('setToken', query.access_token)
     },
     logout: ({ commit }) => {
         commit('setToken', null);
