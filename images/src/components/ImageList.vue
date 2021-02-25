ae94188f25270f57dd8092a4f38cba0cbf1067c8
Alan Leverenz <awleverenz@aol.com>
2021-02-24 21:12:12 -0500

    computed mapGetters to list all images

--- images/src/components/ImageList.vue
+++ images/src/components/ImageList.vue
@@ -1,15 +1,20 @@
 <template>
     <div>
         Image List
+
+        {{ allImageslength }}
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 // created() is a lifecycle method
+// fetchImages action 
+// use computed property to read data into the component and make it accessible from the template
 export default {
     name: 'ImageList',
+    computed: mapGetters(['allImages']),
     methods: mapActions(['fetchImages']),
     created() {
         this.fetchImages();
